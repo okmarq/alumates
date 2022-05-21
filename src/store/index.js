003a467fd0792cdcaa3680401b896e0291041ea1
Joel Okoromi <okmarq@gmail.com>
2022-05-21 08:31:32 +0100

    vuex store updated

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,17 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    showSidebar: false
   },
   getters: {
+    sidebar: state => {
+      return state.showSidebar
+    }
   },
   mutations: {
+    toggleSidebar (state) {
+      state.showSidebar = !state.showSidebar
+    }
   },
   actions: {
   },
