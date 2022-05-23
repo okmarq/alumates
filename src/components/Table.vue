c8a1ee6bc0ff42f7eec2bdc94d5ae31795ed303f
Joel Okoromi <okmarq@gmail.com>
2022-05-23 12:59:07 +0100

    components updated

---
+++ src/components/Table.vue
@@ -0,0 +1,34 @@
+<script setup>
+import TableHead from "@/components/subcomponents/TableHead.vue"
+import TableData from "@/components/subcomponents/TableData.vue"
+</script>
+
+<template>
+	<div>
+		<table>
+			<thead>
+				<tr>
+					<TableHead name="Name" />
+					<TableHead name="Set" />
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<TableData name="Arthur Prescott" />
+					<TableData name="1998" />
+				</tr>
+			</tbody>
+		</table>
+	</div>
+
+	<div class="flex justify-between items-center px-20 gap-5">
+		<span>Page:</span>
+		<span>1</span>
+		<span>2</span>
+		<span>3</span>
+		<span>4</span>
+		<span>5</span>
+		<span>...</span>
+		<span>15</span>
+	</div>
+</template>
---
+++ src/components/UserSchool.vue
@@ -0,0 +1,28 @@
+<script setup>
+import VButton from "@/components/subcomponents/VButton.vue"
+import VInput from "@/components/subcomponents/VInput.vue"
+import { useStore } from "vuex";
+
+const store = useStore()
+</script>
+
+<template>
+	<div>
+		<div>
+			<h2>{{ store.getters.school }}</h2>
+		</div>
+
+		<VButton content="Join alumni network" />
+	</div>
+	<div>
+		<p>All Alumni</p>
+
+		<form>
+			<VInput v-model.trim="search" type="search" placeholder="Search by name or User ID"
+				:class="steps[step].step_name != '3' ? 'pl-9' : ''" />
+
+				<VButton content="Add filters" type="submit" />
+		</form>
+	</div>
+	<div></div>
+</template>
