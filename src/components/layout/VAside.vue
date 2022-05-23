5764b1be43373bbac15159028f0c657a4075a6a0
Joel Okoromi <okmarq@gmail.com>
2022-05-23 12:58:58 +0100

    layouts updated

---
+++ src/components/layout/VAside.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useStore } from 'vuex';
+
+const store = useStore()
+</script>
+
+<template>
+  <div class="backdrop_filter absolute top-0 right-0 z-50" :class="{ 'translate-x-0 transition-all w-screen h-screen': store.getters.sidebar, 'hidden': !store.getters.sidebar }">
+    <div class="bg-white absolute top-0 right-0 flex flex-col gap-12 pt-20" :class="{ 'translate-x-0 transition-all w-40 h-screen': store.getters.sidebar }">
+      <div @click="store.commit('toggleSidebar')">Close</div>
+
+      <div>
+        <div>Login</div>
+
+        <div>Sign Up</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.backdrop_filter {
+  background: rgba(196, 196, 196, 0.46);
+  backdrop-filter: blur(7px);
+}
+</style>
