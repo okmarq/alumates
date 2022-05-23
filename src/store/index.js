2822d3f49de439b007c0ef92661f7e478defd8a0
Joel Okoromi <okmarq@gmail.com>
2022-05-23 12:59:16 +0100

    store updated

--- src/store/index.js
+++ src/store/index.js
@@ -2,16 +2,42 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    showSidebar: false
+    showSidebar: false,
+    alumni: {
+      state: '',
+      city: '',
+      school: ''
+    }
   },
   getters: {
     sidebar: state => {
       return state.showSidebar
+    },
+    alumni: state => {
+      return state.alumni
+    },
+    state: state => {
+      return state.alumni.state
+    },
+    city: state => {
+      return state.alumni.city
+    },
+    school: state => {
+      return state.alumni.school
     }
   },
   mutations: {
-    toggleSidebar (state) {
+    toggleSidebar(state) {
       state.showSidebar = !state.showSidebar
+    },
+    updateState(state, payload) {
+      state.alumni.state = payload
+    },
+    updateCity(state, payload) {
+      state.alumni.city = payload
+    },
+    updateSchool(state, payload) {
+      state.alumni.school = payload
     }
   },
   actions: {
