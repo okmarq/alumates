682a9b684fbb449dd1dfa14596dbb7ec1f39983f
Joel Okoromi <okmarq@gmail.com>
2022-05-28 13:54:54 +0100

    alumni folder renamed

---
+++ src/views/alumni/Layout.vue
@@ -0,0 +1,25 @@
+<script setup>
+import VAside from "@/components/layout/VAside.vue"
+import VFooter from "@/components/layout/VFooter.vue"
+import VHeader from "@/components/layout/VHeader.vue"
+import VMain from "@/components/layout/VMain.vue"
+</script>
+
+<template>
+  <header class="px-4">
+    <VHeader />
+  </header>
+
+  <aside>
+    <VAside />
+  </aside>
+
+  <main class="mt-10 px-4">
+    <VMain />
+    <router-view />
+  </main>
+
+  <footer class="mt-24">
+    <VFooter />
+  </footer>
+</template>
