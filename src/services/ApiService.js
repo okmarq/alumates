01072133ebb43054e95421223c9d880f74639f8d
Joel Okoromi <okmarq@gmail.com>
2022-05-28 16:05:03 +0100

    api services created updated

---
+++ src/services/ApiService.js
@@ -0,0 +1,34 @@
+import axios from "axios"
+
+const apiClient = axios.create({
+	baseURL: '',
+	withCredentials: false,
+	headers: {
+		Accept: 'application/json',
+		'Content-Type': 'application/json'
+	}
+})
+
+export default {
+	register(payload) {
+		return apiClient.post('/register', payload)
+	},
+	login(payload) {
+		return apiClient.post('/login', payload)
+	},
+	forgotPassword(payload) {
+		return apiClient.post('/forgot_password', payload)
+	},
+	resetPassword(payload) {
+		return apiClient.post('/reset_password', payload)
+	},
+	joinAlumni(payload) {
+		return apiClient.post('/join_alumni', payload)
+	},
+	getAlumni(perPage, page) {
+		return apiClient.get('/alumni?_limit=' + perPage + '&_page=' + page)
+	},
+	registerSchool(payload) {
+		return apiClient.post('/register_school', payload)
+	}
+}
