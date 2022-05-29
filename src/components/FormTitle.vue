10ac8a16ca6fea9b3352a9872bc569cbe45160c8
Joel Okoromi <okmarq@gmail.com>
2022-05-29 09:45:59 +0100

    components updated

---
+++ src/components/FormTitle.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <h3 class="font-bold text-xl">{{ title }}</h3>
+</template>
---
+++ src/components/VButton.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  content: {
+    type: String,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <button>{{ content }}</button>
+</template>
