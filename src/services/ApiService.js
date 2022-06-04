bcaf66254d4440fcfbc23f6e4a79cca95c7e8f30
Joel Okoromi <okmarq@gmail.com>
2022-06-04 04:00:04 +0100

    api service updated

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const apiClient = axios.create({
-	baseURL: 'https://api.alumates.com',
+	baseURL: 'https://api-alumates.herokuapp.com',
 	withCredentials: false,
 	headers: {
 		Accept: 'application/json',
