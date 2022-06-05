b9e168f7453f71e2292bebf5696506d5204eb9c6
Joel Okoromi <okmarq@gmail.com>
2022-06-05 14:19:59 +0100

    home updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,18 +7,22 @@ import VHeader from "@/components/layout/VHeader.vue"
 
 <template>
   <header class="px-4">
+    <!-- <slot name="header"></slot> -->
     <VHeader />
   </header>
 
   <aside>
+    <!-- <slot name="aside"></slot> -->
     <VAside />
   </aside>
 
   <main class="mt-10 px-4">
+    <!-- <slot></slot> -->
     <router-view />
   </main>
 
   <footer class="mt-24">
+    <!-- <slot name="footer"></slot> -->
     <VFooter />
   </footer>
 </template>
