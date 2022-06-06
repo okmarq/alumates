34b853a4cd40e9fc26fe3c29a0b7875cc3216fd9
Joel Okoromi <okmarq@gmail.com>
2022-06-06 03:52:23 +0100

    components updated

--- src/components/VInput.vue
+++ src/components/VInput.vue
@@ -17,5 +17,5 @@ defineEmits(['update:modelValue'])
 <template>
   <input :type="type" :placeholder="placeholder" :value="modelValue" v-bind="$attrs"
     @input="$emit('update:modelValue', $event.target.value)"
-    class="py-3 px-4 rounded-full w-full form-input placeholder-[#999999] text-[#151522] appearance-none block focus:outline-none" />
+    class="py-3 px-4 rounded-full w-full placeholder-[#999999] text-[#151522] appearance-none block focus:outline-none" />
 </template>
