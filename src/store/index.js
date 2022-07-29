5eb67d59a5e95098c789dc002dcdddf5a8d5c6f9
Joel Okoromi <okmarq@gmail.com>
2022-07-29 19:23:53 +0100

    store updated

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,17 @@ export default createStore({
     states: [],
     cities: [],
     schools: [],
+    users: [],
+    user: [],
     alumni: {
       state: '',
+      state_id: '',
       city: '',
-      school: ''
+      city_id: '',
+      school: '',
+      school_id: '',
+      group_id: '',
+      user_id: ''
     },
     showSidebar: false,
     showPopup: false,
@@ -30,6 +37,12 @@ export default createStore({
     schools: state => {
       return state.schools
     },
+    users: state => {
+      return state.users
+    },
+    user: state => {
+      return state.user
+    },
     alumni: state => {
       return state.alumni
     },
@@ -41,6 +54,21 @@ export default createStore({
     },
     school: state => {
       return state.alumni.school
+    },
+    stateId: state => {
+      return state.alumni.state_id
+    },
+    cityId: state => {
+      return state.alumni.city_id
+    },
+    schoolId: state => {
+      return state.alumni.school_id
+    },
+    userId: state => {
+      return state.alumni.user_id
+    },
+    groupId: state => {
+      return state.alumni.group_id
     },
     step: state => {
       return state.step
@@ -65,6 +93,12 @@ export default createStore({
     updateSchools(state, payload) {
       state.schools = payload
     },
+    updateUsers(state, payload) {
+      state.users = payload
+    },
+    updateUser(state, payload) {
+      state.user = payload
+    },
     updateState(state, payload) {
       state.alumni.state = payload
     },
@@ -73,6 +107,21 @@ export default createStore({
     },
     updateSchool(state, payload) {
       state.alumni.school = payload
+    },
+    updateStateId(state, payload) {
+      state.alumni.state_id = payload
+    },
+    updateCityId(state, payload) {
+      state.alumni.city_id = payload
+    },
+    updateSchoolId(state, payload) {
+      state.alumni.school_id = payload
+    },
+    updateUserId(state, payload) {
+      state.alumni.user_id = payload
+    },
+    updateGroupId(state, payload) {
+      state.alumni.group_id = payload
     }
   },
   actions: {
