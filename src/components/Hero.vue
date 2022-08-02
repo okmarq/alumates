c1bf4c07c1237ed516f288f7b1e553fb385a7bab
awesomebassey <basseyawesome@gmail.com>
2022-08-02 13:55:14 +0000

    hero page updated

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative -z-10">
-    <h2 class="font-bold text-4xl text-[#151522]">Let's find your alumni</h2>
+    <h2 class="font-bold text-4xl text-[#151522] text-center">Let's connect you with your Alumni</h2>
     <div class="px-4 py-3">
       <p class="text-[#333333]">
         Alumates is a unique social media platform designed specifically for
