368bc46bfcefa7382af8cb3bea27c81628fa96ae
Joel Okoromi <okmarq@gmail.com>
2022-08-03 17:54:08 +0100

    header updated

--- src/components/layout/VHeader.vue
+++ src/components/layout/VHeader.vue
@@ -14,7 +14,7 @@ function getFlgImgUrl(image) {
   return images('./' + image + ".svg")
 }
 const countries = reactive({
-  "Ghana": {
+  "65": {
     id: 65,
     name: "Ghana",
     short_name: "Gha",
@@ -22,7 +22,7 @@ const countries = reactive({
     img: getFlgImgUrl("Gha"),
     class: ''
   },
-  "Nigeria": {
+  "129": {
     id: 129,
     name: "Nigeria",
     short_name: "ng",
@@ -45,26 +45,27 @@ function selectedCountry(data) {
   countryId.id = country.id
   selectedFlag.src = country.img
   selectedFlag.id = country.short_name
+  selectCountry.value = !selectCountry.value
 }
-watchEffect(() => {
-  ApiService.getCountries()
-    .then(function (response) {
-      if (response.status === 200) {
-        response.data.forEach(element => {
-          countries[element.id] = {
-            id: element.id,
-            name: element.name,
-            short_name: element.short_name,
-            img: getFlgImgUrl(element.short_name),
-            class: ''
-          }
-        })
-      }
-    })
-    .catch(function (error) {
-      console.error('Error', error)
-    })
-})
+// watchEffect(() => {
+//   ApiService.getCountries()
+//     .then(function (response) {
+//       if (response.status === 200) {
+//         response.data.forEach(element => {
+//           countries[element.id] = {
+//             id: element.id,
+//             name: element.name,
+//             short_name: element.short_name,
+//             img: getFlgImgUrl(element.short_name),
+//             class: ''
+//           }
+//         })
+//       }
+//     })
+//     .catch(function (error) {
+//       console.error('Error', error)
+//     })
+// })
 watchEffect(() => {
   ApiService.getStates(countryId.id).then(function (response) {
     if (response.status === 200) {
@@ -115,22 +116,18 @@ watchEffect(() => {
         </div>
 
         <div class="overflow-hidden">
-          <div class="shadow-md bg-white rounded-lg w-52 h-56 overflow-y-scroll absolute top-6 -left-4"
+          <div class="shadow-md bg-white rounded-lg w-52 max-h-56 overflow-y-scroll absolute top-6 -left-4"
             :class="{ 'hidden': selectCountry }" id="country_dropdown">
             <Flag v-for="country in countries" v-bind="country" :key="country.id"
               @click="selectedCountry(country.id)" />
-
-            <!-- <Flag name="" img="" @click="selectedCountry(country.id)" />
-
-            <Flag name="" img="" @click="selectedCountry(country.id)" /> -->
           </div>
         </div>
       </div>
     </div>
 
-    <div @click="store.commit('toggleSidebar')" id="hamburger" v-if="page">
+    <!-- <div @click="store.commit('toggleSidebar')" id="hamburger" v-if="page">
       <img src="@/assets/images/hamburger.svg" alt="hamburger"
         class="pr-2 hover:scale-110 cursor-pointer transition-all" />
-    </div>
+    </div> -->
   </div>
 </template>
